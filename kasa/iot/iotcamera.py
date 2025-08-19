0221edc43a791b3dd41b35bfe9eb8062d31191c3
Puxtril <puxtril@protonmail.com>
2025-08-19 19:17:44 -0400

    Wifi commands

--- kasa/iot/iotcamera.py
+++ kasa/iot/iotcamera.py
@@ -8,13 +8,18 @@ from datetime import datetime, tzinfo
 from ..device_type import DeviceType
 from ..deviceconfig import DeviceConfig
 from ..protocols import BaseProtocol
-from .iotdevice import IotDevice
+from ..device import WifiNetwork
+from .iotdevice import IotDevice, KasaException
 
 _LOGGER = logging.getLogger(__name__)
 
 
 class IotCamera(IotDevice):
     """Representation of a TP-Link Camera."""
+
+    # Enumerations
+    key_types = ["NONE", "WEP", "WPA_PSK", "WPA2_PSK", "WEP64", "WEP128", "WPA_AUTO", "WPA3"]
+    cipher_types = ["NONE", "TKIP", "AES", "AUTO", "CCMP"]
 
     def __init__(
         self,
@@ -40,3 +45,33 @@ class IotCamera(IotDevice):
     def is_on(self) -> bool:
         """Return whether device is on."""
         return True
+
+    async def wifi_scan(self) -> list[WifiNetwork]:
+        response = await self._query_helper("smartlife.cam.ipcamera.wireless", "get_ap_list", {"scan":True,"scan_type":"fast"})
+
+        if "ap_info" not in response:
+            raise KasaException(f"Invalid response for wifi scan: {response}")
+
+        network_objs = []
+        for net in response["ap_info"]:
+            
+            key_type_int = -1 # For debugging
+            try:
+                key_type_int = self.key_types.index(net.get("wpa_mode"))
+            except ValueError:
+                pass
+
+            network = WifiNetwork(
+                ssid=net.get("ssid"),
+                key_type=key_type_int,
+                bssid=net.get("bssid"),
+                channel=net.get("channel"),
+                rssi=net.get("rssi"),
+            )
+            network_objs.append(network)
+        return network_objs
+
+    async def wifi_join(self, ssid: str, password: str, keytype: str = "6") -> dict:  # noqa: D202
+        # Should be {"smartlife.cam.ipcamera.wireless":{"set_uplink":{"err_code":0}}}
+        # But often it just returns {}
+        await self._query_helper("smartlife.cam.ipcamera.wireless", "set_uplink", {"encryption":"AUTO", "passphrase":password,"ssid":ssid,"wpa_mode":self.key_types[int(keytype)]})
